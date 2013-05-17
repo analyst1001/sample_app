9e4f1d7fa199728563c049d4fe82c19d247484bb
test <test@C02J7JV6DTY3.group.on>
2013-05-17 19:27:25 +0530

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ group :test do
   gem 'growl', '1.0.3'
   gem 'capybara', '1.1.2'
   gem 'factory_girl_rails', '4.1.0'
+  gem 'cucumber-rails', '1.2.1', :require => false
+  gem 'database_cleaner', '0.7.0'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ SampleApp::Application.routes.draw do
   
 
 
-
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup', to: 'users#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
@@ -10,7 +11,7 @@ SampleApp::Application.routes.draw do
   root to: 'static_pages#home'
   
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
