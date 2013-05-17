55c44bdd3a7d1d3322ff1e90121f53d87e8367c8
test <test@C02J7JV6DTY3.group.on>
2013-05-17 11:10:03 +0530

    End of chapter 7

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-  def gravator_for(user)
+  def gravator_for(user, options = {size:40})
     gravator_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravator_url = "https://secure.gravator.com/avatar/#{gravator_id}"
+    size = options[:size]
+    gravator_url = "https://secure.gravator.com/avatar/#{gravator_id}?s=#{size}"
     image_tag(gravator_url, alt: user.name, class: "gravator")
   end
 end
