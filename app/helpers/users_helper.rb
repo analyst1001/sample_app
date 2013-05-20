df52d6281ae4507350c599da1097c75e2d998861
test <test@C02J7JV6DTY3.group.on>
2013-05-20 12:30:25 +0530

    Finish user model

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
   def gravator_for(user, options = {size:40})
     gravator_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravator_url = "https://secure.gravator.com/avatar/#{gravator_id}?s=#{size}"
+    gravator_url = "http://www.gravatar.com/avatar/#{gravator_id}?s=#{size}"
     image_tag(gravator_url, alt: user.name, class: "gravator")
   end
 end
